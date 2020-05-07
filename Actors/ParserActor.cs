e42caeff953f34bc81af0286d57a7f455efcb10b
백지혜 <jihye.paik@mirero.co.kr>
2020-05-07 10:44:57 +0900

    message 수정

--- Actors/ParserActor.cs
+++ Actors/ParserActor.cs
@@ -37,25 +37,59 @@ namespace BLUECATS.ToastNotifier.Actors
                     DateTimeZoneHandling = DateTimeZoneHandling.Local,
                 });
 
-                string localtime = json["@timestamp"].ToString("yyyy-MM-dd HH:mm:ss")+"("+ json["@timestamp"].ToString("ddd")+ ")";
-                string title = "[" + json.jsonMessage["site"] + "]" + Regex.Replace(json.jsonMessage["product_names"].ToString(), @"[|\n|\r|\s]", string.Empty)
-                                + System.Environment.NewLine + json.jsonMessage["monitor_name"] + ": "+json.jsonMessage["trigger"];
-                string summary = Regex.Replace(json.jsonMessage["host"].ToString(), @"[|\n|\r|\s]", string.Empty);
+                string localtime = json["@timestamp"].ToString("yyyy-MM-dd HH:mm:ss") + "(" + json["@timestamp"].ToString("ddd") + ")";
+                string title = System.Environment.NewLine + json.jsonMessage["monitor_name"] + ": " + json.jsonMessage["trigger_name"];
+                string hosts = GetHosts(json.jsonMessage["host_name"].Value);
 
                 var sb = new StringBuilder();
                 StringBuilder message = sb.AppendLine(string.Format($"{localtime}"))
                     .AppendLine(title)
-                    .AppendLine("[Summary] " + summary);
+                    .AppendLine(hosts);
 
                 string sendMsg = message.ToString();
                 if (sendMsg.Length > msgLength)
                     sendMsg = string.Concat(message.ToString().Remove(msgLength), "...");
-     
+
                 notificationActor.Tell(
-                    ((NotificationLevel)Enum.Parse(typeof(NotificationLevel), json.jsonMessage["severity"].ToString(), true), sendMsg)
+                    ((NotificationLevel)Enum.Parse(typeof(NotificationLevel), GetSeverity(json.jsonMessage["severity"].ToString()), true), sendMsg)
                 );
             });
+        }
 
+        private string GetHosts(string value)
+        {
+            if (value == "{}")
+                return "테스트 입니다.";
+            var hosts = value.Trim('{', '}').Split(',').Select(x => x.Split('=')).ToDictionary(x => x[0], x => x[1]);
+            return string.Join(", ", hosts.Select(x => x.Value));
+        }
+
+        private string GetSeverity(string severtityLevel)
+        {
+            var severity = string.Empty;
+            switch (severtityLevel)
+            {
+                case "1":
+                    severity = NotificationLevel.Debug.ToString();
+                    break;
+                case "2":
+                    severity = NotificationLevel.Info.ToString();
+                    break;
+                case "3":
+                    severity = NotificationLevel.Warning.ToString();
+                    break;
+                case "4":
+                    severity = NotificationLevel.Error.ToString();
+                    break;
+                case "5":
+                    severity = NotificationLevel.Error.ToString();
+                    break;
+                default:
+                    severity = NotificationLevel.Error.ToString();
+                    break;
+            }
+
+            return severity;
         }
     }
 }
