e9858c562a2c1e5c969e9ed158f20e83799b2cfe
Chris Shim <seungyongshim@users.noreply.github.com>
2020-05-12 09:26:27 +0900

    테스트 코드 추가 (#4)
    * 테스트 추가
    - ValidationAuthoritySpec
    
    * ci 수정
    
    * CI에 테스트 추가
    
    * 현재 시스템 문화권 문제 수정
    
    * cs 인코딩 문제 수정
    
    * Release CI 수정
    
    Co-authored-by: 심승용 <seungyong.shim@mirero.co.kr>

---
+++ ToastNotifier.Tests/ValidationAuthoritySpec.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Akka.Configuration;
+using BLUECATS.ToastNotifier;
+using FluentAssertions;
+using Xunit;
+
+namespace ToastNotifier.Tests
+{
+    public class ValidationAuthoritySpec
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void Should_Pass(int level)
+        {
+            // arrange
+            var config = ConfigurationFactory.ParseString($"ui.notification.authority-level={level}");
+            var validationAuthority = typeof(App).GetMethod("ValidationAuthority", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            Action act = () => validationAuthority.Invoke(null, new []{config});
+
+            // act, assert
+            act.Should().NotThrow();
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(6)]
+        [InlineData(7)]
+        [InlineData(8)]
+        [InlineData(9)]
+        public void Should_Throw_Exception(int level)
+        {
+            // arrange
+            var config = ConfigurationFactory.ParseString($"ui.notification.authority-level={level}");
+            var validationAuthority = typeof(App).GetMethod("ValidationAuthority", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+            Action act = () => validationAuthority.Invoke(null, new[] { config });
+
+            // act, assert
+            act.Should().Throw<Exception>()
+                .WithInnerException<Exception>()
+                .WithMessage("authority-level은 1~5까지 지정할 수 있습니다.");
+        }
+    }
+}
